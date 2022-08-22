652a10e60d483455582c4aa12b9076ed3bc20563
Kevin Weaver <kevinweaver2@gmail.com>
2022-08-22 16:07:31 -0700

    Updated greeter contract addresses

--- contracts/ethereum/GreeterL1.sol
+++ contracts/ethereum/GreeterL1.sol
@@ -6,9 +6,9 @@ import { ICrossDomainMessenger } from
     "@eth-optimism/contracts/libraries/bridge/ICrossDomainMessenger.sol";
 
 contract GreeterL1 {
-    address crossDomainMessengerAddr = 0x4361d0F75A0186C05f971c566dC6bEa5957483fD;
+    address crossDomainMessengerAddr = 0x5086d1eEF304eb5284A0f6720f79403b4e9bE294;
 
-    address greeterL2Addr = 0xD4c204223d6F1Dfad0b7a0b05BB0bCaB6665e0c9;
+    address greeterL2Addr = 0xC0836cCc8FBa87637e782Dde6e6572aD624fb984;
 
     function setGreeting(string calldata _greeting) public {
         bytes memory message;
